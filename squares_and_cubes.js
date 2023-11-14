65fc54f66e99566929b260dd6f0745dcf97b75de
jijek <maseug@icloud.com>
2023-11-14 16:12:15 +0600

    Update squares_and_cubes.js

--- squares_and_cubes.js
+++ squares_and_cubes.js
@@ -6,3 +6,15 @@ const solution = (first, second) => {
 };
 
 module.exports = solution;
+
+const _ = require("lodash");
+const R = require("rambda");
+
+const solution = (first, second) => {
+  if (first === 0 && second === 0) return true;
+
+  const div = second / first;
+  return div * div === first && div*div*div === second;
+};
+
+module.exports = solution;
